31d1f5b0346e102bc180598372da7079e307a6db
Shane F <shanevi29@gmail.com>
2021-03-20 19:10:45 -0700

    update middleware

--- server/server.js
+++ server/server.js
@@ -20,9 +20,9 @@ app.use(express.json());
 
 // mount custom middleware for cors
 app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods', '*');
-	res.setHeader('Access-Control-Allow-Headers', '*');
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
 	next();
 });
 
